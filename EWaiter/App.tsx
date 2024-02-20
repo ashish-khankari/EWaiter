58d697be4256931481723a50e3cb3943a4e84084
Ashish Khankari <ashishkhankari0927@gmail.com>
2024-02-20 22:39:54 +0530

    Navigation Imports

--- EWaiter/App.tsx
+++ EWaiter/App.tsx
@@ -1,4 +1,12 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
 import React from 'react';
+import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
